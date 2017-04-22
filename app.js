24713dd53917483393e72e32226292a85b6fb05a
loren pabst <loren pabst>
2017-04-22 11:00:43 -0600

    player moves, health bar shows up

--- app.js
+++ app.js
@@ -2,12 +2,38 @@
 $(document).ready(function(){
 
 var player = $('#player');
+var game_box = $('.game_box');
+var health = 200;
+var rain = $('<div class="rain"></div>');
 
-$(window).keypressed(function(e){
-    if(e.which === 13){
-        player.css('bottom', '40px');
+game_box.append(rain);
+
+game_box.hover(
+    function(){
+        player.css('background', 'cyan');
+    },
+    function(){
+        player.css('background', '#ccc');
     }
+);
+
+$( document ).on( "mousemove", function( event ) {
+    var x = event.pageX - 445;
+    var y = event.pageY - 135;
+    if (x > 10 && x < 370){
+        player.css('left', x);
+    }
+    if (y > 10 && y < 368){
+        player.css('top', y);
+    }
+    if (x > 400){
+        health -= 1;
+        $('.health').css('width', health);
+    }
+  
 });
+
+
 
 
 
